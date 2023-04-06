7158ee234bc215ce4952682b868b7bc2ce8d7b25
banan-bayan <88555875+banan-bayan@users.noreply.github.com>
2023-04-06 16:47:13 +0500

    add lint

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,20 +1,17 @@
 #!/usr/bin/env node
 
 
-
 let startNumber = Math.floor(Math.random() * 10) + 1;
 let progressionNumber = Math.floor(Math.random() * 10) + 1;
 let stringOfNumbers = [];
 
-for (let i = 0; stringOfNumbers.length < 24; i++) {
+for (let i = 0; stringOfNumbers.length  < 24; i ++) {
    startNumber += progressionNumber;
-   stringOfNumbers += startNumber + ' ';
+   stringOfNumbers +=   startNumber + ' ';
 };
 
 let arrayOfNumbers = stringOfNumbers.split(' ');
 arrayOfNumbers.pop();
-
-//console.log(arrayOfNumbers);
 
 
 const getRndInteger = (min, max) =>  Math.floor(Math.random() * (max - min) ) + min;
@@ -22,14 +19,19 @@ let mysteryDots = '..';
 let correctIndexAnswer = getRndInteger(0, arrayOfNumbers.length);
 let correctNumber = arrayOfNumbers[correctIndexAnswer];
 console.log(correctNumber);
-delete arrayOfNumbers[correctIndexAnswer];
+delete arrayOfNumbers[correctIndexAnswer]; 
 arrayOfNumbers.splice(correctIndexAnswer, 1, mysteryDots);
 console.log(arrayOfNumbers);
 let stringOfNumbersWithoutAnswer = arrayOfNumbers.join(' ');
 
 
-let userName = prompt('Welcome to the Brain Games! May I have your name?')
+const userName = prompt('Welcome to the Brain Games! May I have your name?');
 alert('Hello, ' + userName + '!');
-let question = prompt('What number is missing in the progression? \n Question: ' + stringOfNumbersWithoutAnswer, '');
+let counterQuestions = 0;
 
-  (question == correctNumber) ? alert('Your answer: ' + question + ' Correct!' ) : alert('Your answer: ' + "'" + question  + "' " + '\n' + "'" + question + "'" + ' is wrong answer ;(. Correct answer was ' + "'" + correctNumber + "'" + '\n' + " Let's try again, " + userName + '!')
+let question =   prompt('What number is missing in the progression? \n Question: ' + stringOfNumbersWithoutAnswer, '');
+if (question === correctNumber) {
+  alert('Your answer: ' + question + ' Correct!');
+} else {
+   alert('Your answer: ' + "'" + question  + "' " + '\n' + "'" + question + "'" + ' is wrong answer ;(. Correct answer was ' + "'" + correctNumber + "'" + '\n' + " Let's try again, " + userName + '!');
+}
