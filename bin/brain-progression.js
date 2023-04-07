f0f47f197a913bb4e1a11e1a9c3bb85ae792c010
banan-bayan <88555875+banan-bayan@users.noreply.github.com>
2023-04-07 14:45:31 +0500

    add first question

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,9 +1,39 @@
 #!/usr/bin/env node
-const readlineSync = require('readline-sync');
+import readlineSync from 'readline-sync';
 
-const user = readlineSync.question('May I have your name? ');
-console.log(`Hi ${user}!`);
-const favFood = readlineSync.question('What is your favorite food? ', {
-  hideEchoBack: true,
-});
-console.log(`Oh, ${user} loves ${favFood}!`);
+let startNumber = Math.floor(Math.random() * 10) + 1;
+const progressionNumber = Math.floor(Math.random() * 10) + 1;
+let stringOfNumbers = [];
+
+for (let i = 0; stringOfNumbers.length < 24; i += 1) {
+  startNumber += progressionNumber;
+  stringOfNumbers += `${startNumber} `;
+}
+
+const arrayOfNumbers = stringOfNumbers.split(' ');
+arrayOfNumbers.pop();
+
+// console.log(arrayOfNumbers);
+
+const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const mysteryDots = '..';
+const correctIndexAnswer = getRndInteger(0, arrayOfNumbers.length);
+const correctNumber = arrayOfNumbers[correctIndexAnswer];
+
+// console.log(correctNumber);
+
+delete arrayOfNumbers[correctIndexAnswer];
+arrayOfNumbers.splice(correctIndexAnswer, 1, mysteryDots);
+
+// console.log(arrayOfNumbers);
+
+const stringOfNumbersWithoutAnswer = arrayOfNumbers.join(' ');
+
+const userName = readlineSync.question('Welcome to the Brain Games! May I have your name? ');
+console.log(`Hello ${userName}!`);
+const vopros = readlineSync.question(`What number is missing in the progression? \nQuestion: ${stringOfNumbersWithoutAnswer} \n`);
+if (vopros === correctNumber) {
+  console.log(`Your answer: ${vopros} Correct!`);
+} else {
+  console.log('Your answer: ' + `'${vopros}' ` + '\n' + `'${vopros}'` + ' is wrong answer ;(. Correct answer was ' + `'${correctNumber}'` + '\n' + ` Let's try again, ${userName}!`);
+}
