4b0fb26e7928bb038336a2ca75bf7d64aa02eeac
banan-bayan <88555875+banan-bayan@users.noreply.github.com>
2023-04-07 17:36:46 +0500

    add all questions

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,39 +1,55 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-let startNumber = Math.floor(Math.random() * 10) + 1;
-const progressionNumber = Math.floor(Math.random() * 10) + 1;
-let stringOfNumbers = [];
+const getAnswerAndQuestion = () => {
+  let startNumber = Math.floor(Math.random() * 10) + 1;
+  const progressionNumber = Math.floor(Math.random() * 10) + 1;
+  let stringOfNumbers = [];
 
-for (let i = 0; stringOfNumbers.length < 24; i += 1) {
-  startNumber += progressionNumber;
-  stringOfNumbers += `${startNumber} `;
-}
+  for (let i = 0; stringOfNumbers.length < 24; i += 1) {
+    startNumber += progressionNumber;
+    stringOfNumbers += `${startNumber} `;
+  }
 
-const arrayOfNumbers = stringOfNumbers.split(' ');
-arrayOfNumbers.pop();
+  const arrayOfNumbers = stringOfNumbers.split(' ');
+  arrayOfNumbers.pop();
 
-// console.log(arrayOfNumbers);
+  const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+  const mysteryDots = '..';
+  const correctIndexAnswer = getRndInteger(0, arrayOfNumbers.length);
+  const correctNumber = arrayOfNumbers[correctIndexAnswer];
 
-const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-const mysteryDots = '..';
-const correctIndexAnswer = getRndInteger(0, arrayOfNumbers.length);
-const correctNumber = arrayOfNumbers[correctIndexAnswer];
+  delete arrayOfNumbers[correctIndexAnswer];
+  arrayOfNumbers.splice(correctIndexAnswer, 1, mysteryDots);
 
-// console.log(correctNumber);
+  const stringOfNumbersWithoutAnswer = arrayOfNumbers.join(' ');
 
-delete arrayOfNumbers[correctIndexAnswer];
-arrayOfNumbers.splice(correctIndexAnswer, 1, mysteryDots);
-
-// console.log(arrayOfNumbers);
-
-const stringOfNumbersWithoutAnswer = arrayOfNumbers.join(' ');
+  return {
+    correctNumber,
+    qustionString: stringOfNumbersWithoutAnswer,
+  };
+};
+const firstTrueAnswerAndQuestion = getAnswerAndQuestion();
 
 const userName = readlineSync.question('Welcome to the Brain Games! May I have your name? ');
 console.log(`Hello ${userName}!`);
-const vopros = readlineSync.question(`What number is missing in the progression? \nQuestion: ${stringOfNumbersWithoutAnswer} \n`);
-if (vopros === correctNumber) {
-  console.log(`Your answer: ${vopros} Correct!`);
+let answerUser = readlineSync.question(`What number is missing in the progression? \nQuestion: ${firstTrueAnswerAndQuestion.qustionString} \n`);
+if (answerUser === firstTrueAnswerAndQuestion.correctNumber) {
+  console.log(`Your answer: ${answerUser} Correct!`);
 } else {
-  console.log(`Your answer: ${vopros} \n ${vopros} is wrong answer ;(. Correct answer was ${correctNumber} Lets try again, ${userName}!`);
+  console.log(`Your answer: ${answerUser} \n ${answerUser} is wrong answer ;(. Correct answer was ${firstTrueAnswerAndQuestion.correctNumber} Lets try again, ${userName}!`);
+}
+const secondTrueAnswerAndQuestion = getAnswerAndQuestion();
+answerUser = readlineSync.question(`What number is missing in the progression? \nQuestion: ${secondTrueAnswerAndQuestion.qustionString} \n`);
+if (answerUser === secondTrueAnswerAndQuestion.correctNumber) {
+  console.log(`Your answer: ${answerUser} Correct!`);
+} else {
+  console.log(`Your answer: ${answerUser} \n ${answerUser} is wrong answer ;(. Correct answer was ${secondTrueAnswerAndQuestion.correctNumber} Lets try again, ${userName}!`);
+}
+const thirdTrueAnswerAndQuestion = getAnswerAndQuestion();
+answerUser = readlineSync.question(`What number is missing in the progression? \nQuestion: ${thirdTrueAnswerAndQuestion.qustionString} \n`);
+if (answerUser === thirdTrueAnswerAndQuestion.correctNumber) {
+  console.log(`Your answer: ${answerUser} Correct!`);
+} else {
+  console.log(`Your answer: ${answerUser} \n ${answerUser} is wrong answer ;(. Correct answer was ${thirdTrueAnswerAndQuestion.correctNumber} Lets try again, ${userName}!`);
 }
