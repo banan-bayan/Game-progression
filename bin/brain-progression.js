ec90e37b668606afc2ff95de45d8b2baae551ec5
banan-bayan <88555875+banan-bayan@users.noreply.github.com>
2023-04-08 11:20:18 +0500

    fix

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,27 +1,23 @@
 #!/usr/bin/env node
+
 import readlineSync from 'readline-sync';
 
 const getAnswerAndQuestion = () => {
   let startNumber = Math.floor(Math.random() * 10) + 1;
   const progressionNumber = Math.floor(Math.random() * 10) + 1;
   let stringOfNumbers = [];
-
   for (let i = 0; stringOfNumbers.length < 24; i += 1) {
     startNumber += progressionNumber;
     stringOfNumbers += `${startNumber} `;
   }
-
   const arrayOfNumbers = stringOfNumbers.split(' ');
   arrayOfNumbers.pop();
-
   const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min)) + min;
   const mysteryDots = '..';
   const correctIndexAnswer = getRndInteger(0, arrayOfNumbers.length);
   const correctNumber = arrayOfNumbers[correctIndexAnswer];
-
   delete arrayOfNumbers[correctIndexAnswer];
   arrayOfNumbers.splice(correctIndexAnswer, 1, mysteryDots);
-
   const stringOfNumbersWithoutAnswer = arrayOfNumbers.join(' ');
 
   return {
@@ -29,6 +25,7 @@ const getAnswerAndQuestion = () => {
     qustionString: stringOfNumbersWithoutAnswer,
   };
 };
+
 const firstTrueAnswerAndQuestion = getAnswerAndQuestion();
 
 const userName = readlineSync.question('Welcome to the Brain Games! May I have your name? ');
@@ -43,13 +40,13 @@ if (answerUser === firstTrueAnswerAndQuestion.correctNumber) {
     const thirdTrueAnswerAndQuestion = getAnswerAndQuestion();
     answerUser = readlineSync.question(`What number is missing in the progression? \nQuestion: ${thirdTrueAnswerAndQuestion.qustionString} \n`);
     if (answerUser === thirdTrueAnswerAndQuestion.correctNumber) {
-      console.log(`Your answer: ${answerUser} Correct! \n Congratulations, ${userName}!`);
+      console.log(`Your answer: ${answerUser} Correct! \nCongratulations, ${userName}!`);
     } else {
-      console.log(`Your answer: ' ${answerUser} ' \n '${answerUser}' is wrong answer ;(. Correct answer was ${thirdTrueAnswerAndQuestion.correctNumber} Lets try again, ${userName}!`);
+      console.log(`Your answer: ${answerUser}\n '${answerUser}' is wrong answer ;(. Correct answer was '${thirdTrueAnswerAndQuestion.correctNumber}' Lets try again, ${userName}!`);
     }
   } else {
-    console.log(`Your answer: '${answerUser}' \n '${answerUser}' is wrong answer ;(. Correct answer was ${secondTrueAnswerAndQuestion.correctNumber} Lets try again, ${userName}!`);
+    console.log(`Your answer: ${answerUser} \n '${answerUser}' is wrong answer ;(. Correct answer was '${secondTrueAnswerAndQuestion.correctNumber}' Lets try again, ${userName}!`);
   }
 } else {
-  console.log(`Your answer: '${answerUser}' \n '${answerUser}' is wrong answer ;(. Correct answer was ${firstTrueAnswerAndQuestion.correctNumber} Lets try again, ${userName}!`);
+  console.log(`Your answer: ${answerUser} \n '${answerUser}' is wrong answer ;(. Correct answer was '${firstTrueAnswerAndQuestion.correctNumber}' Lets try again, ${userName}!`);
 }
